6d7499c91e7312784a4af69f73e7ad82a1068059
swathi22003343 <120440439+swathi22003343@users.noreply.github.com>
2024-11-02 14:25:16 +0530

    Update Histogram processing of an image.py

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -3,47 +3,45 @@
 
 # In[1]:Write your code to find the histogram of gray scale image and color image channels
 
-
 import cv2
 import matplotlib.pyplot as plt
-gray_image = cv2.imread("gray image of flower.jpg")
-color_image = cv2.imread()
-cv2.imshow("Gray Image",gray_image)
+gray_image = cv2.imread('gray_image.jpeg')
+color_image = cv2.imread('color_image.jpg')
+cv2.imshow("Gray image",gray_image)
 cv2.imshow("color image",color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
+
 # In[2]:Display the histogram of gray scale image and any one channel histogram from color image
 
 
+
+import numpy as np
+gray_image=cv2.imread('gray_image.jpeg')
 import matplotlib.pyplot as plt 
-grayscale_image=cv2.imread("gray image of flower.jpg")
-colourscale_image=cv2.imread("color image of flower.jpg")
-hist=cv2.calcHist(grayscale_image,[0],None,[255],[0,255])
-hist1=cv2.calcHist()
+gray_hist=cv2.calcHist(gray_image,[0],None,[255],[0,255])
 plt.figure()
-plt.title("Histogram")
-plt.xlabel("Colorscale value")
-plt.ylabel("pixel count")
-plt.stem(color_hist)
+plt.imshow(gray_image)
 plt.show()
-
-
+plt.title("Histogram")
+plt.xlabel("Grayscale value")
+plt.ylabel("pixel count")
+plt.stem(gray_hist)
+plt.show()
 
 # In[3]:Write the code to perform histogram equalization of the image. 
 
 
 
 import cv2
-import matplotlib.pyplot as plt 
-gi=cv2.imread("gray image of flower.jpg",0)
-colorscale=cv2.imread("color image of flower.jpg")
-g=cv2.resize(gi,(500,400))
-equ=cv2.equalizeHist(gi)
-
-
-
+gray_image = cv2.imread("gray_image.jpeg",0)
+cv2.imshow('Grey Scale Image',gray_image)
+equ = cv2.equalizeHist(gray_image)
+cv2.imshow("Equalized Image",equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
